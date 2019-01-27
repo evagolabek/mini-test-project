26a5e5187cbe882b01b4ddff9c15b663c3624c4a
evagolabek <golabek.eva@gmail.com>
2019-01-27 21:04:06 +0100

    blogs component plus routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'blogs', component: BlogsComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarMenuComponent
+    SidebarMenuComponent,
+    BlogsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
