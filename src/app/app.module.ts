9e070765fb68a2cd52e81af2f02db39f69755927
evagolabek <golabek.eva@gmail.com>
2019-01-27 21:22:34 +0100

    data-table component with angular material example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,36 @@ import { AppComponent } from './app.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { Routes, RouterModule } from '@angular/router';
+import { DataTableComponent } from './data-table/data-table.component';
+
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
+  MatSortModule, MatTableModule } from "@angular/material";
 
 
 const routes: Routes = [
   {path: 'blogs', component: BlogsComponent},
+  {path: 'data-table', component: DataTableComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarMenuComponent,
-    BlogsComponent
+    BlogsComponent,
+    DataTableComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    //BrowserAnimationsModule,
+    //HttpClientModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+        MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
