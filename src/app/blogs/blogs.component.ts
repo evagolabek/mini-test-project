5bd88530343e5147080411bdd764e8062e730364
evagolabek <golabek.eva@gmail.com>
2019-02-10 12:04:49 +0100

    filter fix

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -8,13 +8,12 @@ export interface blogInfo {
 }
 
 const rawData: blogInfo[] = [
-  {blogger: 'MyCoolComp', type: 'Cool', status: 'active'},
-  {blogger: 'TheDestiny', type: 'Not Cool', status: 'active'},
-  {blogger: 'Test', type: 'Cool', status: 'inactive'},
-  {blogger: 'Test2', type: 'Not Cool', status: 'active'},
+  {blogger: 'MyCoolComp', type: 'Cool', status: 'Active'},
+  {blogger: 'TheDestiny', type: 'Average', status: 'Active'},
+  {blogger: 'Test', type: 'Cool', status: 'Inactive'},
+  {blogger: 'Test', type: 'Average', status: 'Active'},
+  {blogger: 'Test', type: 'Mediocre', status: 'Active'},
 ];
-
-
 
 @Component({
   selector: 'app-blogs',
@@ -29,12 +28,18 @@ export class BlogsComponent implements OnInit {
   notCool: string; 
 
   displayedColumns: string[] = ['blogger', 'type', 'status'];
-  typeFilters = [];
-  filteredData;
+  Filters = {type:[], status:[], blogger:[]};
+  filteredData: blogInfo[];
+  //filteredDataType: blogInfo[];
+  //filteredDataStatus: blogInfo[];
   dataSource;
 
   renderTable() {
-    this.filteredData = rawData.filter(x => this.typeFilters.includes(x.type))
+    this.filteredData = rawData.filter(x => (this.Filters.type.includes(x.type)) && (this.Filters.status.includes(x.status)) && (this.Filters.blogger.includes(x.blogger)))
+
+    // this.filteredDataType = rawData.filter(x => this.Filters.type.includes(x.type))
+
+    // this.filteredDataStatus = this.filteredDataType.filter(x => this.Filters.status.includes(x.status))
 
     this.dataSource = this.filteredData;
   }
@@ -44,24 +49,38 @@ export class BlogsComponent implements OnInit {
   ngOnInit() {
   }
 
-  typeChecked(value: string){
-    console.log(value);
+  Checked(column: string, value: string){
+    console.log(column, value);
 
-    // check if there is an element with this value in the array and return its index
-    var idx = this.typeFilters.indexOf(value);
+    if (column == 'type') {
+      // check if there is an element with this value in the array and return its index
+      var idx = this.Filters.type.indexOf(value);
 
-     // is already in filters array (not -1), then remove it
-     if (idx > -1) {
-       this.typeFilters.splice(idx, 1);
-     }
+      // is already in filters array (not -1), then remove it
+      if (idx > -1) {
+        this.Filters.type.splice(idx, 1);
+      }
 
-     // if newly selected, then add it
-     else {
-       this.typeFilters.push(value);
-     }
+      // if newly selected, then add it
+      else {
+        this.Filters.type.push(value);
+      }
 
-    console.log(this.typeFilters)
-
+      console.log(this.Filters.type)
+    }
+    else if (column == 'blogger') {
+      var idx = this.Filters.blogger.indexOf(value);
+      if (idx > -1) {this.Filters.blogger.splice(idx, 1);}
+      else {this.Filters.blogger.push(value);}
+      console.log(this.Filters.blogger)
+    }
+    else if (column == 'status') {
+      var idx = this.Filters.status.indexOf(value);
+      if (idx > -1) {this.Filters.status.splice(idx, 1);}
+      else {this.Filters.status.push(value);}
+      console.log(this.Filters.status)
+    }
+    
     this.renderTable();
  }
 }
